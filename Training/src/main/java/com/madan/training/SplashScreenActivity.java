294e131f8659eeb6fe2b4ed7dca0246da41a986a
Madan Parameswaran <mparameswaran@citrisys.com>
2013-12-10 16:04:11 +0530

    ADD: splash screen function

--- Training/src/main/java/com/madan/training/SplashScreenActivity.java
+++ Training/src/main/java/com/madan/training/SplashScreenActivity.java
@@ -1,18 +1,28 @@
 package com.madan.training;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * Created by madan on 10/12/13.
  */
 public class SplashScreenActivity extends Activity {
 
-    private static int timeOut = 3000;
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, TrainingMainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
