870b1c6d2425270aedb8a80993a6acb53bb64d81
Madan Parameswaran <mparameswaran@citrisys.com>
2013-12-10 11:21:36 +0530

    ADD: splash screen background

--- Training/src/main/java/com/madan/training/SplashScreenActivity.java
+++ Training/src/main/java/com/madan/training/SplashScreenActivity.java
@@ -1,10 +1,18 @@
 package com.madan.training;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by madan on 10/12/13.
  */
 public class SplashScreenActivity extends Activity {
 
+    private static int timeOut = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
 }
